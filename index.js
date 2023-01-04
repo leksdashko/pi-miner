a20a193271df1bf22ee4160ec11ae3fe6d008c66
Oleksandr Dashko <leksdashko@gmail.com>
2023-01-03 16:19:53 -0800

    faq finished

--- index.js
+++ index.js
@@ -2,11 +2,42 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-app.use(express.json());
+const mainConfig = {
+	links: {
+		android:process.env.ANDROID_URL,
+		ios:process.env.IOS_URL,
+		mac: process.env.MAC_URL,
+		windows: process.env.WINDOWS_URL,
+		official: process.env.OFFICIAL_DOMAIN
+	},
+	title: 'Pi Network Portal',
+	invitationCode: process.env.INVITATION_CODE,
+}
 
-app.get('/', async (req, res) => {
-	return res.status(200).json({"status":"okay"});
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+	res.render('pages/index', mainConfig);
 });
+
+app.get('/about', function(req, res) {
+	res.render('pages/about', mainConfig);
+});
+
+app.get('/faq', function(req, res) {
+	res.render('pages/faq', mainConfig);
+});
+
+app.get('/node', function(req, res) {
+	res.render('pages/node', mainConfig);
+});
+
+app.get('*', function(req, res){
+  res.render('pages/404', mainConfig);
+});
+
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log('Server started on PORT = ' + PORT));
